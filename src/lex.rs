c5c5fdc2637dc5a2ecca7dc148cb14b000da7ae9
Connor Horman <chorman64@gmail.com>
2025-11-30 19:22:23 -0500

    Lex string literals (probably)

--- src/lex.rs
+++ src/lex.rs
@@ -1,7 +1,8 @@
-use logos::{Logos, Span};
+use logos::{Lexer, Logos, Span};
 
 #[derive(Logos)]
-pub enum Token<'src> {
+#[logos(error(Span, |lex| lex.span()))]
+pub enum Token {
     #[token("and")]
     And,
     #[token("break")]
@@ -119,8 +120,50 @@ pub enum Token<'src> {
     #[regex("0x[0-9A-Fa-f]*(\\.[0-9A-Fa-f]*)?([pP][1-9][0-9]*)?", |lexer| lexer.span())]
     Number(Span),
 
+    #[regex("\"([^\"\\\n\r]|(\\\\[\\\\nr'\"abtv])|(\\\\u\\{[0-9A-Fa-f]{1,8}\\})|(\\\\z[[:space:]]*)|(\\\\[0-9]{1,3})|(\\\\x[0-9A-Fa-f]{2}))\"", |lexer| lexer.span())]
+    StringLiteral(Span),
+
+    #[regex("\\[=*\\[", parse_raw_string)]
+    RawString(Span),
+
     #[regex("[[:space:]]+", logos::skip)]
     #[regex("--[\\[\\n]*\\n", logos::skip)]
     #[regex("--\\[[^\\]]*\\]", logos::skip)]
     Whitespace,
+}
+
+fn parse_raw_string(lex: &mut Lexer<Token>) -> Result<Span, Span> {
+    let n = lex.slice().len().strict_sub(2);
+
+    let mut rem = lex.remainder();
+    let mut total_len = 0;
+
+    'a: while let Some(x) = rem.find('[') {
+        total_len += x;
+
+        let l = &rem[x..];
+
+        rem = l;
+
+        let b = l.as_bytes();
+
+        if b.len() < (n + 2) {
+            lex.bump(total_len);
+            return Err(lex.span());
+        }
+
+        for b in b.iter().skip(1).take(n) {
+            if *b != b'=' {
+                continue 'a;
+            }
+        }
+
+        if b[1 + n] == b']' {
+            lex.bump(total_len + 2 + n);
+            return Ok(lex.span());
+        }
+    }
+
+    lex.bump(total_len + rem.len());
+    Err(lex.span())
 }
